53d27bd863b07acdb75387e7fabeb7109d560c06
an tran <antran.teachhert@gmail.com>
2024-10-13 18:59:16 +0700

    fix(SEO): update size image of apple-touch-icon

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -20,7 +20,7 @@ const Head = () => {
       <link rel="icon" href="/images/logo.png" />
       <meta name="theme-color" content="#0A0A0A" />
       <meta name="color-scheme" content="dark light" />
-      <link rel="apple-touch-icon" href={image} sizes="57x57" />
+      <link rel="apple-touch-icon" href={image} />
       <link rel="canonical" href={urlWebsite+ pathname} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="title" content={titleContent} />
